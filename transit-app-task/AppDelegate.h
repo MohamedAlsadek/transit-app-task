48a79dbb1aa2d067ba3ce2490c9b3ec94285e89c
MohamedAlsadek <MohamedAlsadek@s.otventures.local>
2016-01-13 14:45:45 +0200

    Initial first project push

---
+++ transit-app-task/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  transit-app-task
+//
+//  Created by Mohamed Alsadek on 1/13/16.
+//  Copyright © 2016 Mohamed Alsadek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
